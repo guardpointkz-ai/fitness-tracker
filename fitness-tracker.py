8dbbc336b828dc5ef26c27b7ce92800867517469
Alexandr <guardpoint.kz@gmail.com>
2026-09-21 10:24:33 +0500

    Дополнил код, исправил стиль

--- fitness-tracker.py
+++ fitness-tracker.py
@@ -1,5 +1,15 @@
 print("Здравствуйте!")
-steps = int(input ("Шаги "))
-calories = int(input ("Калории "))
-water_ml = int(input ("Вода "))
-sleep_hours = float(input ("Сон "))
+steps = int(input("Шаги за день "))
+calories = int(input("Калории "))
+water_ml = int(input("Вода мл "))
+sleep_hours = float(input("Сон ч "))
+#Цели
+water_goal = 2000
+steps_goal = 10000
+#Расчет процентов
+water_percent = water_ml / water_goal * 100
+steps_percent = steps / steps_goal * 100
+#Итоговый отчет
+print("==ОТЧЕТ==")
+print(f'''Процент от цели по воде {water_percent} %
+Процент от цели по шагам в день {steps_percent} %''')
