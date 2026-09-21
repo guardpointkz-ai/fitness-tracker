a4b80404912dadd64bc237d07f44e3a41dd6b851
Alexandr <guardpoint.kz@gmail.com>
2026-09-21 10:43:57 +0500

    Дополнил код, применил round к процентам

--- fitness-tracker.py
+++ fitness-tracker.py
@@ -3,13 +3,19 @@ steps = int(input("Шаги за день "))
 calories = int(input("Калории "))
 water_ml = int(input("Вода мл "))
 sleep_hours = float(input("Сон ч "))
-#Цели
+# Цели
 water_goal = 2000
 steps_goal = 10000
-#Расчет процентов
-water_percent = water_ml / water_goal * 100
-steps_percent = steps / steps_goal * 100
-#Итоговый отчет
+calories_goal = 2000
+sleep_hours_goal = 8
+# Расчет процентов
+water_percent = round(water_ml / water_goal * 100, 1)
+steps_percent = round(steps / steps_goal * 100, 1)
+calories_percent = round(calories / calories_goal * 100, 1)
+sleep_hours_percent = round(sleep_hours / sleep_hours_goal * 100, 1)
+# Итоговый отчет
 print("==ОТЧЕТ==")
-print(f'''Процент от цели по воде {water_percent} %
-Процент от цели по шагам в день {steps_percent} %''')
+print(f'''Процент от цели по воде {water_percent}%
+Процент от цели по шагам в день {steps_percent}%
+Процент от цели по калориям {calories_percent}%
+Процент от цели по сну {sleep_hours_percent}%''')
